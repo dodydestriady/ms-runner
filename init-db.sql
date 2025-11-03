577d366896d813fc0b89a0f4d1b0a07fde754b77
Dody Destriady <dodydestriady@gmail.com>
2025-11-03 15:00:59 +0700

    Initialize productdb and orderdb with privileges
    Create databases for products and orders with privileges.

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,4 @@
--- File ini akan dijalankan sekali saat container postgres pertama kali dibuat
 CREATE DATABASE productdb;
 CREATE DATABASE orderdb;
--- Berikan hak akses ke user 'postgres' (opsional, tapi baik untuk kejelasan)
 GRANT ALL PRIVILEGES ON DATABASE productdb TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE orderdb TO postgres;
